981450e718bdce612789f64dc2685586a2b03c9b
Elladan81 <heladan@hotmail.fr>
2019-05-28 12:04:52 +0200

    Update constants.py
    create var for the game core and picture

--- constants.py
+++ constants.py
@@ -5,3 +5,19 @@ the title of the window, its icon, the number of sprites on a width, the size of
 and therefore the size of the window (multiplies the last two constants ) 
 
 """
+
+#window setting
+num_sprite_len = 15
+sprite_size = 30
+window_size = num_sprite_len * sprite_size
+
+#window configuration
+title_window = "McGyver Labyrinthe"
+img_icon = "images/MacGyver.png"
+
+#game picture
+img_home = ""
+img_background = ""
+img_wall = ""
+img_start= ""
+img_finish = ""
