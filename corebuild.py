fd9edd054587f313678a8210ebd9738015ea6217
Elladan81 <heladan@hotmail.fr>
2019-05-28 09:41:30 +0200

    Create corebuild.py
    create corebuild.py
    file containng game core and mechanics

---
+++ corebuild.py
@@ -0,0 +1,12 @@
+""" 
+Game core file : this file contains the mechanics of the game
+
+McGyver game
+
+Game in which McGyver must cross a labyrinth and defeat his guardian. Along the way, he must find a straw and a needle to make a sarbacanne.
+
+I know, it looks crazy
+
+Script : Python
+Files : corebuild.py, classes.py, configuration.py images, sond and music.
+"""
