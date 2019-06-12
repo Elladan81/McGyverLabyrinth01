855f76b1ec6a88f93605f7a45ce28de578781eb9
Elladan81 <heladan@hotmail.fr>
2019-06-12 11:38:57 +0200

    Udapte classes.py
    Minor corrections

--- classes.py
+++ classes.py
@@ -14,7 +14,7 @@ Items to collect : creates item with a random position, a current image, how to 
 
 import pygame
 from pygame.locals import *
-from constantes import *
+from constants import *
 
 class Level:
     """this class creates the lvl"""
@@ -41,20 +41,20 @@ class Level:
         start = pygame.image.load(img_start).convert()
         finish = pygame.image.load(img_finish).convert_alpha()
 
-    num_line = 0
-    for line in self.strucutre:
-        num_case = 0
-        for sprite in line:
-            x = num_case*sprite_size
-            y = num_line*sprite_size
-            if sprite =='m':
-                window.blit(wall, (x,y))
-            elif sprite =='s':
-                window.blit(start, (x,y))
-            elif sprite =='f':
-                window.blit(finish, (x,y))
-            num_case += 1
-        num_line +=1
+        num_line = 0
+        for line in self.strucutre:
+            num_case = 0
+            for sprite in line:
+                x = num_case*sprite_size
+                y = num_line*sprite_size
+                if sprite =='m':
+                    window.blit(wall, (x,y))
+                elif sprite =='s':
+                    window.blit(start, (x,y))
+                elif sprite =='f':
+                    window.blit(finish, (x,y))
+                num_case += 1
+            num_line +=1
 
 class MacGuyver:
     def __init__(self, right, left, up, down, Level):
@@ -64,12 +64,12 @@ class MacGuyver:
         self.up = pygame.image_load(up).convert_alpha()
         self.down = pygame.image_load(down).convert_alpha()
         #character position
-        slef.case_x = 0
+        self.case_x = 0
         self.case_y = 0
         self.x = 0
         self.y =0
         #start Direction
-        slef.direction = self.right
+        self.direction = self.right
         #lvl start
         self.level = Level
     
@@ -82,7 +82,7 @@ class MacGuyver:
                 #we chack that is not a wall
                 if self.level.strucutre[self.case_y][self.case_x+1] != 'm':
                     #move one step
-                    slef.case_x +=1
+                    self.case_x +=1
                     #calculate real position in pixel
                     self.x = self.case_x*sprite_size
             #img in the good position
@@ -91,20 +91,20 @@ class MacGuyver:
         if direction == 'left':
             if self.case_x > 0:
                 if self.level.strucutre[self.case_y][self.case_x-1] != 'm':
-                    slef.case_x -=1
+                    self.case_x -=1
                     self.x = self.case_x*sprite_size
             self.direction = self.left
         #move top
         if direction == 'up':
             if self.case_x > 0:
                 if self.level.strucutre[self.case_y-1][self.case_x] != 'm':
-                    slef.case_y -=1
+                    self.case_y -=1
                     self.x = self.case_x*sprite_size
             self.direction = self.up
             #move down
         if direction == 'down':
             if self.case_y < (num_sprite_len -1):
                 if self.level.strucutre[self.case_y+1][self.case_x] != 'm':
-                    slef.case_y +=1
+                    self.case_y +=1
                     self.x = self.case_x*sprite_size
             self.direction = self.down
