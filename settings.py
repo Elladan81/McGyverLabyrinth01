8a75194917a4e61e23a627336cf16581cd884741
Elladan81 <heladan@hotmail.fr>
2019-07-01 15:49:10 +0200

    Update Settings.py
    Create a Class Settings
    reviewing all code.

--- settings.py
+++ settings.py
@@ -1,26 +1,29 @@
 """ 
 
-My settings file contains the addresses of the images, for an easier modification, 
+My constant file contains the addresses of the images, for an easier modification, 
 the title of the window, its icon, the number of sprites on a width, the size of the sprites, 
 and therefore the size of the window (multiplies the last two constants ) 
 
 """
+class Settings:
 
-#window setting
-num_sprite_len = 15
-sprite_size = 30
-window_size = num_sprite_len * sprite_size
+    def __init__(self):
+    #window setting
+        self.num_sprite_len = 15
+        self.sprite_size = 30
+        self.window_size = self.num_sprite_len * self.sprite_size
 
-#window configuration
-window_title = "McGyver Labyrinthe"
-img_icon = "images/MacGyver.png"
+        #window configuration
+        self.window_title = "McGyver Labyrinthe"
+        self.img_icon = "images/MacGyver.png"
 
-#game picture
-img_home = "images/home.png"
-img_background = "images/background.png"
-img_wall = "images/wall.png"
-img_start= "images/start.png"
-img_finish = "images/finish.png"
-img_needle = "images/needle.png"
-img_straw = "images/straw.png"
-img_chloro = "images/choloroform.png"
+        #game picture
+        self.img_macgyver = "images/macgyver.png"
+        self.img_home = "images/home.png"
+        self.img_background = "images/background.png"
+        self.img_wall = "images/wall.png"
+        self.img_start= "images/start.png"
+        self.img_finish = "images/finish.png"
+        self.img_needle = "images/needle.png"
+        self.img_straw = "images/straw.png"
+        self.img_ether = "images/ether.png"
